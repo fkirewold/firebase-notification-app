fc97fadd218b30f5ba91276bcd586befa6adec22
fkirewold <addimanzerihun@gmail.com>
2025-10-02 16:27:52 +0300

    feat: create flutter local notification for foreground and open app

---
+++ lib/core/utils/local_notification_service.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+      bool initialized = false;
+
+  static final LocalNotificationService _instance = LocalNotificationService._internal();
+  factory LocalNotificationService() {
+    return _instance;
+  }
+  LocalNotificationService._internal();
+  
+ Future<void> init() async {
+  if(initialized)
+  {
+    return;
+  }
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  initialized = true;
+
+ }
+
+ Future<void> showNotification(RemoteMessage message) async{
+  await init();
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+    'call_notifications',
+    'Call Notifications',
+    channelDescription: 'General push notifications from the top',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+  const NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    message.notification?.title ?? 'No Title',
+    message.notification?.body ?? 'No Body',
+    platformChannelSpecifics,
+    payload: 'item x',
+  );
+
+
+ }
+
+}
